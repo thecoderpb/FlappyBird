47fe3eee5b2cd21f7129bf2101806d37ac3c8151
Pratik Byathnal(Trainee) <m9507260@mindtree.com>
2020-01-28 23:43:52 +0530

    Initial commit

--- core/src/com/pratik/flappybird/FlappyBird.java
+++ core/src/com/pratik/flappybird/FlappyBird.java
@@ -9,7 +9,7 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class FlappyBird extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
-	
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
@@ -18,13 +18,15 @@ public class FlappyBird extends ApplicationAdapter {
 
 	@Override
 	public void render () {
+
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
+
 	}
-	
+
 	@Override
 	public void dispose () {
 		batch.dispose();
