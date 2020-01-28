766a01114d33fa28bd4d04984a46be43282693d1
Pratik Byathnal(Trainee) <m9507260@mindtree.com>
2020-01-29 02:23:56 +0530

    ShapeRender Circle

--- core/src/com/pratik/flappybird/FlappyBird.java
+++ core/src/com/pratik/flappybird/FlappyBird.java
@@ -2,8 +2,11 @@ package com.pratik.flappybird;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Circle;
 
 import java.util.Random;
 
@@ -19,10 +22,14 @@ public class FlappyBird extends ApplicationAdapter {
 	private int gameState = 0;
 
 	private Random random;
-	private int gap = 100;
+	private int gap = 0;
 
-	private int[] tubeX,offsets;
+	private int[] tubeX;
+	private float[] offsets;
 	private int distanceBetweenTubes;
+
+	Circle birdCircle;
+	ShapeRenderer shapeRenderer;
 
 	@Override
 	public void create () {
@@ -44,16 +51,19 @@ public class FlappyBird extends ApplicationAdapter {
 
 
 		tubeX = new int[4];
-		offsets = new int[4];
+		offsets = new float[4];
 		distanceBetweenTubes = Gdx.graphics.getWidth()*3/4;    // for every distance 0.75 of screen width, new tube is generated;
 
 		for(int i = 0; i<4 ; i++){
 
 			tubeX[i] = Gdx.graphics.getWidth()/2-topTube.getWidth()/2 + Gdx.graphics.getWidth()+ i*distanceBetweenTubes;
-			offsets[i] = random.nextInt(Gdx.graphics.getHeight()*3/4);
+			offsets[i] = random.nextInt(Gdx.graphics.getHeight()*3/4)*0.5f;
 		}
 
 
+
+		birdCircle = new Circle();
+		shapeRenderer = new ShapeRenderer();
 
 
 	}
@@ -74,7 +84,7 @@ public class FlappyBird extends ApplicationAdapter {
 
 		if(gameState == 1){
 
-			if(flapState == 0){
+			if(flapState == 0){           // Check Flap State
 				flapState=1;
 			}else {
 				flapState=0;
@@ -83,7 +93,7 @@ public class FlappyBird extends ApplicationAdapter {
 			if(birdY>0 || velocity<0){
 
 				velocity = velocity +gravity;
-				birdY = birdY - velocity;
+				birdY = birdY - velocity;            // Makes the bird flap
 
 			}
 
@@ -91,9 +101,9 @@ public class FlappyBird extends ApplicationAdapter {
 			  This for loop checks if tube has moved away from screen. If yes then generate a tube
 			 */
 
-			for (int i = 0; i < 4; i++) {
+			for (int i = 0; i < 4; i++) {          //Generates set of 4 tubes
 
-				if(tubeX[i]<-topTube.getWidth()){
+				if(tubeX[i]<-topTube.getWidth()){   // If tube is out of the screen move it to the other side of the screen
 
 					tubeX[i] += 4*distanceBetweenTubes; // 4 is the number of tubes
 
@@ -103,6 +113,7 @@ public class FlappyBird extends ApplicationAdapter {
 
 				}
 
+				//draw tubes
 				batch.draw(topTube,tubeX[i],Gdx.graphics.getHeight()/2f + gap/2f + offsets[i]);
 				batch.draw(bottomTube,tubeX[i],-Gdx.graphics.getHeight()/2f + gap/2f  + offsets[i]);
 
@@ -111,9 +122,19 @@ public class FlappyBird extends ApplicationAdapter {
 
 		}
 
-
-
 		batch.draw(birds[flapState], Gdx.graphics.getWidth()/2f - birds[flapState].getWidth()/2f,birdY);
+
+		birdCircle.set(Gdx.graphics.getWidth()/2,birdY +birds[flapState].getHeight()/2f,birds[flapState].getHeight()/2);
+
+
+		//ShapeRenderer Class
+		shapeRenderer.begin(ShapeRenderer.ShapeType.Filled);
+		shapeRenderer.setColor(Color.RED);
+		shapeRenderer.circle(birdCircle.x,birdCircle.y,birdCircle.radius);
+		shapeRenderer.end();
+
+
+
 
 		batch.end();
 
